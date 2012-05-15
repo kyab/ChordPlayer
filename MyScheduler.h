003b197a5c25bf3b66c35eb0170f4de343ea89a1
kyab <kyossi212@gmail.com>
2012-05-15 23:27:38 +0900

    update some files

---
+++ MyScheduler.h
@@ -0,0 +1,13 @@
+//
+//  MyScheduler.h
+//  ChordPlayer
+//
+//  Created by 吉岡 紘二 on 12/05/13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyScheduler : NSObject
+
+@end
