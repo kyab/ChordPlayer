2043db9eef533913b95a889256830b192983fbdf
kyab <kyossi212@gmail.com>
2012-05-18 01:47:25 +0900

    implementing transpose function

--- ChordPlayer/Controller.rb
+++ ChordPlayer/Controller.rb
@@ -10,22 +10,107 @@ class NSString
 	def play
 		Chords.new(self).play
 	end
+
 end
+
+def baseName2NoteNumber(baseName)
+	case baseName
+	when "C"
+		63
+	when "C#", "Db"
+		64
+	when "D", "D"
+		65
+	when "D#", "Eb"
+		66
+	when "E"
+		67
+	when "F"
+		68
+	when "F#","Gb"
+		69
+	when "G"
+		70
+	when "G#", "Ab"
+		71
+	when "A"
+		72
+	when "A#","Bb"
+		73
+	when "B"
+		74
+	else
+		raise "#{baseName} can't be recognized as base note name."
+	end
+end
+
+def transpose(baseName, num)
+	chords_array= ["C","C#","D","D#","E","F","F#","G", "G#","A","A#","B"] #=>size = 12
+	fromC = chords_array.index(baseName)
+	raise "basename error. \"#{baseName}\"" if fromC == nil
+	
+	if (fromC + num) > chords_array.size-1
+		num -= chords_array.size - fromC
+		fromC = 0
+	elsif (fromC + num) < 0
+		num += fromC
+		fromC = chords_array.size
+	end
+	
+	#raise IndexError for out of range transpose value
+	ret = chords_array.fetch(fromC + num)		
+	 
+end
+
+puts transpose("C", 3) #=>D# or Eb
+puts transpose("C#",3) #=>E
+puts transpose("F#", 4) #=>A#
+puts transpose("F#", 5) #=>B
+puts transpose("A#", 6) #=>E
+puts transpose("A#", 7) #=>F
+puts transpose("C#", -1) #=>C
+puts transpose("C#", -2) #=>B
+puts transpose("C#", -11) #=> D
+puts transpose("C",12) #=>C
+puts transpose("C",-12) #=>C
+
+begin
+	puts transpose("B", 13) #=>IndexError
+rescue IndexError =>e
+	puts "OK IndexError"
+end
+begin 
+	puts transpose("B", -13) #=>IndexError
+	puts "nono, here"
+rescue IndexError =>e
+	puts "OK IndexError"
+end
+
 
 class Chord
 	attr_accessor :notes, :duration
-	def initialize(base,on,subpart,duration = 4)
-		puts "new Chord #{base},#{subpart},/#{on},duration = #{duration}"
+	attr_accessor :base_str, :on_str, :subpart_str
+	
+	def initialize(base_str, on_str, subpart_str, duration = 4)
+		puts "new Chord #{base_str},#{subpart_str},/#{on_str},duration = #{duration}"
 		
+		@base_str = base_str
+		@on_str = on_str
+		@subpart_str = subpart_str
 		@duration = duration
+		parse
+	end
+	
+
+	def parse
 		@notes = []
-		if (on != "")
-			@notes << baseName2NoteNumber(on)
+		if (!on_str.empty?)
+			@notes << baseName2NoteNumber(@on_str)
 		else
-			@notes << baseName2NoteNumber(base)
+			@notes << baseName2NoteNumber(@base_str)
 		end
 		
-		relatives = case subpart		#you shold have knowledge to understand this!
+		relatives = case @subpart_str		#you shold have knowledge to understand this!
 		when ""
 			[4,7]
 		when "7"
@@ -61,16 +146,43 @@ class Chord
 		when "7(-13)"
 			[4,7,10,20]
 		else
-			raise "\"#{subpart}\"not supported"
+			raise "\"#{@subpart_str}\"not supported"
 		end
 		
 		relatives.each do |r|
-			@notes << baseName2NoteNumber(base) + r
+			@notes << baseName2NoteNumber(@base_str) + r
 		end
 		
-			
+	end
+
+	
+	def to_s
+		ret = "#{@base_str}#{@subpart_str}"
+		ret << "/#{@on_str}" unless @on_str.empty?
+		ret << case @duration
+		when 1
+			""
+		when 2
+			" "
+		when 4
+			"  "
+		end
+	end
+	
+	def transpose!(num)
+		@duration = @duration
+		@base_str = transpose(@base_str, num)
+		@on_str = transpose(@on_str, num) unless @on_str.empty?
+		@subpart_str = @subpart_str
+		parse
+		#more to be done...
+		self
 	end
 end
+
+
+p Chord.new("C#","","m7",2).to_s
+p Chord.new("C#","","m7",2).transpose!(10).to_s
 
 class Chords
 
@@ -129,7 +241,7 @@ class Chords
 				subpart_and_duration = line[i..nextCode_index-1]
 				subpart = ""
 				duration = " "
-				if (subpart_and_duration =~ /([^ -]*)(.*)/)			#" ","-"以外と
+				if (subpart_and_duration =~ /([^ -]*)(.*)/)
 					subpart = $1
 					if ($2.include?("-9") || $2.include?("-13"))
 						if ($2[1..$2.size-1] =~ /([^ -]*).*/)
@@ -185,37 +297,8 @@ class Chords
 			
 end
 
-def baseName2NoteNumber(baseName)
-	case baseName
-	when "C"
-		63
-	when "C#", "Db"
-		64
-	when "D", "D"
-		65
-	when "D#", "Eb"
-		66
-	when "E"
-		67
-	when "F"
-		68
-	when "F#","Gb"
-		69
-	when "G"
-		70
-	when "G#", "Ab"
-		71
-	when "A"
-		72
-	when "A#","Bb"
-		73
-	when "B"
-		74
-	else
-		raise "#{baseName} can't be recognized as base note name."
-	end
-end
 	
+=begin
 def noteName2noteNumber(noteName)
 
 	case noteName
@@ -247,7 +330,7 @@ def noteName2noteNumber(noteName)
 		raise "#{noteName} can't be recognized as note name."
 	end
 end
-
+=end
 
 $soundDelegate = SoundDelegate.new
 $scheduler = MyScheduler.sharedMyScheduler
@@ -258,7 +341,6 @@ class Controller
 					   
 	def awakeFromNib
 		puts"ChordPlayer awaken"
-
 		
 		@field.setFont(NSFont.fontWithName("Osaka-Mono", size:18))
 		@field.string = <<-END.gsub(/^\t\t\t/,"")
@@ -290,6 +372,7 @@ class Controller
 	
 		#Here is what things happen.
 		@field.string.play
+				
 	end
 
 end
